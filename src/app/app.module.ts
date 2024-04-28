6053ec716f1abd1d325edb2c1c43754a1cf1a4df
Alvin Consular <alvin.consular.accenture.com>
2024-04-29 00:28:59 +0800

    modified:   angular.json 	new file:   firebase.json 	new file:   ngsw-config.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/board/board.component.html 	new file:   src/app/board/board.component.scss 	new file:   src/app/board/board.component.spec.ts 	new file:   src/app/board/board.component.ts 	new file:   src/app/square/square.component.html 	new file:   src/app/square/square.component.scss 	new file:   src/app/square/square.component.spec.ts 	new file:   src/app/square/square.component.ts 	new file:   src/assets/icons/icon-128x128.png 	new file:   src/assets/icons/icon-144x144.png 	new file:   src/assets/icons/icon-152x152.png 	new file:   src/assets/icons/icon-192x192.png 	new file:   src/assets/icons/icon-384x384.png 	new file:   src/assets/icons/icon-512x512.png 	new file:   src/assets/icons/icon-72x72.png 	new file:   src/assets/icons/icon-96x96.png 	modified:   src/index.html 	new file:   src/manifest.webmanifest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, isDevMode } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ServiceWorkerModule } from '@angular/service-worker';
+// import { ServiceWorkerModule } from '@angular/service-worker';
+// import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    // ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
